f73a64d80cf3ad4928fb6456d088b2858d3be14c
Víctor Luévano <victoradrian_2003@outlook.com>
2025-10-06 17:17:53 -0600

    Solución de errores

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -25,6 +25,8 @@ class UserFactory extends Factory
     {
         return [
             'name' => fake()->name(),
+            'first_name' => fake()->firstName(),
+            'last_name' => fake()->lastName(),
             'email' => fake()->unique()->safeEmail(),
             'email_verified_at' => now(),
             'password' => static::$password ??= Hash::make('password'),
