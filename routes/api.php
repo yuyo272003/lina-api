eb1b910013ec34ec89a27c8b371cf03ef4853802
Víctor Luévano <victoradrian_2003@outlook.com>
2025-10-08 12:03:49 -0600

    Se muestran los datos del estudiante

--- routes/api.php
+++ routes/api.php
@@ -2,9 +2,12 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Api\EstudianteController;
 
 Route::middleware(['auth:sanctum'])->get('/user', function (Request $request) {
     return $request->user();
 });
 
 require __DIR__.'/auth.php';
+
+Route::middleware('auth:sanctum')->get('/perfil-estudiante', [EstudianteController::class, 'getProfile']);
